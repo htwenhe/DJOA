0b03a2a0bb3a0a11d83716630b0314bbbfde6ae5
htwenhe <htwenhe@hotmail.com>
2017-11-03 19:40:51 +0800

    json填充select表单 modal框，时间，日前控件 ajax 提交表单，存储，返回，关闭，刷新。

--- whoa/settings.py
+++ whoa/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
